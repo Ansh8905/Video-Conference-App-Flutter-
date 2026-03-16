8f67e974c88dda769f63dbaf09d5d359d8814054
Ansh8905 <anshpandey1906@outook.com>
2026-03-17 00:41:46 +0530

    Add project files

---
+++ lib/resources/auth_methods.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:myp/utils/utils.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Stream<User?> get authChanges => _auth.authStateChanges();
+  User get user => _auth.currentUser!;
+
+  Future<bool> signInWithGoogle(BuildContext context) async {
+    bool res = false;
+    try {
+      final account = await GoogleSignIn.instance.authenticate();
+      final String? idToken = account.authentication.idToken;
+      final authz = await account.authorizationClient
+          .authorizationForScopes(['openid', 'email', 'profile']);
+      final String? accessToken = authz?.accessToken;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: accessToken,
+        idToken: idToken,
+      );
+
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+
+      User? user = userCredential.user;
+
+      if (user != null) {
+        if (userCredential.additionalUserInfo!.isNewUser) {
+          await _firestore.collection('users').doc(user.uid).set({
+            'username': user.displayName,
+            'uid': user.uid,
+            'profilePhoto': user.photoURL,
+          });
+        }
+        res = true;
+      }
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!);
+      res = false;
+    }
+    return res;
+  }
+
+  void signOut() async {
+    try {
+      _auth.signOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+}
